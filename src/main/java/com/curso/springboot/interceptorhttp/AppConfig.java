bb0d6d9c893d73b195150b27d070b28a96038337
ctobonpe <ctobonpe@gmail.com>
2025-05-18 17:21:35 -0500

    interceptor de horario

--- src/main/java/com/curso/springboot/interceptorhttp/AppConfig.java
+++ src/main/java/com/curso/springboot/interceptorhttp/AppConfig.java
@@ -3,21 +3,33 @@ package com.curso.springboot.interceptorhttp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@PropertySources({
+	@PropertySource(value="classpath:config/app.properties", encoding = "UTF-8")	
+})
 public class AppConfig implements WebMvcConfigurer {
 
 	@Autowired()
 	@Qualifier("loadingTimeInterceptor")
-	private HandlerInterceptor loadingTimeInterceptor;
+	private HandlerInterceptor loadingTimeInterceptor;	
+	
+	@Autowired
+	@Qualifier("scheduleInterceptor")
+	private HandlerInterceptor scheduleInterceptor;
 	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		//registry.addInterceptor(loadingTimeInterceptor).addPathPatterns("/app/int");
-		registry.addInterceptor(loadingTimeInterceptor).excludePathPatterns("/app/bar","/app/baz");
+		registry.addInterceptor(loadingTimeInterceptor).addPathPatterns("/app/int");
+		//registry.addInterceptor(loadingTimeInterceptor).excludePathPatterns("/app/bar","/app/baz");
+		
+		registry.addInterceptor(scheduleInterceptor).addPathPatterns("/app/schedule");
+		
 	}
 
 	
--- src/main/java/com/curso/springboot/interceptorhttp/controllers/AppController.java
+++ src/main/java/com/curso/springboot/interceptorhttp/controllers/AppController.java
@@ -1,11 +1,18 @@
 package com.curso.springboot.interceptorhttp.controllers;
 
+//import java.text.ParseException;
+//import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
+//import java.util.TimeZone;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("app")
@@ -27,5 +34,23 @@ public class AppController {
 	public Map<String,Object> baz(){		
 		return Collections.singletonMap("message", "handler baz del controlador");
 	}
-
+	
+	@GetMapping("/schedule")
+	public Map<String,Object> schedule(HttpServletRequest request){	
+		
+		/*SimpleDateFormat isoFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		isoFormat.setTimeZone(TimeZone.getTimeZone("America/Bogota"));
+		Date date = null;
+		try {
+			date = isoFormat.parse(new Date().toString());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}*/
+		
+		Map<String, Object> data = new HashMap<>();
+		data.put("greeting", request.getAttribute("greeting"));
+		data.put("message", "Bienvenidos al sistema de atencion!");
+		data.put("time", /*date*/ new Date().toString());
+		return data;
+	}
 }
